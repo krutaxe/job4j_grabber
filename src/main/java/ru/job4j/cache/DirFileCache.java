5289737776212215794d23dc24ac0813e5b6c1ab
Nikolaev Dmitriy <90854753+krutaxe@users.noreply.github.com>
2022-02-19 21:37:34 +0300

    1. Реализация кеша на SoftReference [#1592]

---
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -0,0 +1,25 @@
+package ru.job4j.cache;
+
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DirFileCache extends AbstractCache<String, String> {
+
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key) {
+        String rsl = null;
+        try {
+            rsl = Files.readString(Path.of(cachingDir, key));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+}
---
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -0,0 +1,21 @@
+package ru.job4j.cache;
+
+import java.util.Scanner;
+
+public class Emulator {
+    public static void main(String[] args) {
+        DirFileCache dirFileCache1 = new DirFileCache("C:\\projects\\cache");
+        String fileNames = "Names.txt";
+        String fileAddress = "Address.txt";
+        System.out.println(dirFileCache1.get(fileAddress));
+        System.out.println(dirFileCache1.get(fileNames));
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter cached directory:");
+        String dir = in.nextLine();
+        DirFileCache dirFileCache2 = new DirFileCache(dir);
+        System.out.println("Enter cached file:");
+        String file = in.nextLine();
+        System.out.println(dirFileCache2.get(file));
+    }
+}
