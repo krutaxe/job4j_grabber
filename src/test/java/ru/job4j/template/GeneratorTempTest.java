4e7a8862384fc5709d732560804a241cd0b03ced
Nikolaev Dmitriy <90854753+krutaxe@users.noreply.github.com>
2022-02-20 13:12:39 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTempTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class GeneratorTempTest {
+
+    @Ignore
+    @Test
+    public void whenGeneratorTestOne() {
+        Generator generator = new GeneratorTemp();
+        Map<String, String> tempMap = Map.of("name", "Dima", "subject", "you");
+        String template = "I am a ${name}, Who are ${subject}?";
+        String rsl = generator.produce(template, tempMap);
+        assertThat(rsl, is("I am a Dima, Who are you?"));
+    }
+
+    @Ignore
+    @Test
+    public void whenGeneratorTestTwo() {
+        Generator generator = new GeneratorTemp();
+        Map<String, String> tempMap = Map.of("?", "Hello", "!", "name");
+        String template = "${?}, my name is Misha, what is your ${!}? ";
+        String rsl = generator.produce(template, tempMap);
+        assertThat(rsl, is("Hello, my name is Misha, what is your name? "));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void wrongKeys () {
+        Generator generator = new GeneratorTemp();
+        Map<String, String> tempMap = Map.of("user", "Hello", "login", "name");
+        String template = "${?}, my name is Misha, what is your ${!}? ";
+        String rsl = generator.produce(template, tempMap);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void extraKeys () {
+        Generator generator = new GeneratorTemp();
+        Map<String, String> tempMap = Map.of("?", "Hello", "!", "name", "@", "are");
+        String template = "${?}, my name is Misha, what is your ${!}? ";
+        String rsl = generator.produce(template, tempMap);
+    }
+}
