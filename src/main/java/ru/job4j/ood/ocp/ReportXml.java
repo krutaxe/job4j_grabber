b341f912606e47395f01741607a649b7253b8303
Nikolaev Dmitriy <90854753+krutaxe@users.noreply.github.com>
2022-03-01 23:29:09 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #269830]

--- src/main/java/ru/job4j/ood/ocp/ReportXml.java
+++ src/main/java/ru/job4j/ood/ocp/ReportXml.java
@@ -1,37 +1,24 @@
 package ru.job4j.ood.ocp;
 
 import ru.job4j.ood.srp.Employee;
-import ru.job4j.ood.srp.Report;
-import ru.job4j.ood.srp.Store;
+import ru.job4j.ood.srp.ReportAll;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.util.List;
-import java.util.function.Predicate;
 
-public class ReportXml implements Report {
-    private Store store;
-
-    public ReportXml(Store store) {
-        this.store = store;
-    }
-
+public class ReportXml implements ReportAll {
     @Override
-    public String generate(Predicate<Employee> filter) {
-        List<Employee> employees = store.findBy(filter);
+    public String report(List<Employee> list) {
         String xml = "";
-
         try (StringWriter writer = new StringWriter()) {
-            JAXBContext context = JAXBContext.newInstance(Employee.class);
+            JAXBContext context = JAXBContext.newInstance(Employees.class);
             Marshaller marshaller = context.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            for (Employee e: employees) {
-                marshaller.marshal(e, writer);
-            }
+            marshaller.marshal(new Employees(list), writer);
             xml = writer.getBuffer().toString();
-        } catch (JAXBException | IOException e) {
+            System.out.println(xml);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return xml;
--- src/test/java/ru/job4j/ood/ocp/ReportXmlTest.java
+++ src/test/java/ru/job4j/ood/ocp/ReportXmlTest.java
@@ -4,13 +4,10 @@ import org.junit.Test;
 import ru.job4j.ood.srp.Employee;
 import ru.job4j.ood.srp.MemStore;
 import ru.job4j.ood.srp.Report;
-
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import ru.job4j.ood.srp.ReportEngine;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class ReportXmlTest {
@@ -18,22 +15,28 @@ public class ReportXmlTest {
     @Test
     public void reportXmlTest() {
         MemStore store = new MemStore();
-        Calendar calendar = new GregorianCalendar(2022, Calendar.FEBRUARY, 26);
-        calendar.setTimeZone(TimeZone.getTimeZone(ZoneOffset.of("+3")));
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        String date = dateFormat.format(calendar.getTime());
         Employee employee = new Employee("Roma", calendar, calendar, 300);
         store.add(employee);
-        Report engine = new ReportXml(store);
-        String rsl = engine.generate(e -> true);
-        StringBuilder expected = new StringBuilder()
-                .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n")
-                .append("<employee>\n")
-                .append("    <name>").append(employee.getName()).append("</name>\n")
-                .append("    <hired>").append("2022-02-26T00:00:00")
-                .append("+03:00").append("</hired>\n")
-                .append("    <fired>").append("2022-02-26T00:00:00")
-                .append("+03:00").append("</fired>\n")
-                .append("    <salary>").append(employee.getSalary()).append("</salary>\n")
-                .append("</employee>\n");
-        assertEquals(expected.toString(), rsl);
+        ReportXml report = new ReportXml();
+        Report engine = new ReportEngine(store, report);
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
+                + "\n<employees>"
+                + "\n"
+                + "    <employees>"
+                + "\n"
+                + "        <fired>" + date + "</fired>"
+                + "\n"
+                + "        <hired>" + date + "</hired>"
+                + "\n"
+                + "        <name>" + employee.getName() + "</name>"
+                + "\n"
+                + "        <salary>" + employee.getSalary() + "</salary>"
+                + "\n"
+                + "    </employees>"
+                + "\n</employees>\n";
+        assertThat(engine.generate(e -> true), is(expected));
     }
 }
