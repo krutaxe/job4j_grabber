7bb1a27a11659ce4f07233e810abcb94e7b9535e
Nikolaev Dmitriy <90854753+krutaxe@users.noreply.github.com>
2022-04-25 23:08:03 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -1,11 +1,14 @@
 package ru.job4j.quartz;
 
-
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -13,44 +16,56 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-
-    private static int interval;
-
-    public static void config() {
-        Properties properties = new Properties();
+    public static void main(String[] args) {
         try (InputStream in = AlertRabbit.class.getClassLoader()
                 .getResourceAsStream("rabbit.properties")) {
-        properties.load(in);
-        interval = Integer.parseInt(properties.getProperty("rabbit.interval"));
-        } catch (IOException e) {
+            Properties cfg = new Properties();
+            cfg.load(in);
+            Class.forName(cfg.getProperty("driver"));
+            try (Connection cnn = DriverManager.getConnection(
+               cfg.getProperty("url"),
+               cfg.getProperty("username"),
+               cfg.getProperty("password"))) {
+                Scheduler scheduler = StdSchedulerFactory
+                        .getDefaultScheduler();
+                scheduler.start();
+                JobDataMap data = new JobDataMap();
+                data.put("cnn", cnn);
+                JobDetail job = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(Integer.parseInt(cfg
+                                .getProperty("rabbit.interval")))
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            } catch (SQLException | SchedulerException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
-        }
-        System.out.println(interval);
-    }
-
-    public static void main(String[] args) {
-        config();
-        try {
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(interval)
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
+        public void execute(JobExecutionContext context)  {
+            Connection connection = (Connection) context.getJobDetail()
+                    .getJobDataMap().get("connect");
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "insert into rabbit (create_date) values (?)")) {
+                statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                statement.execute();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
